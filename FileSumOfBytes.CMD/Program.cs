6f215b95fd165df0aa34a91cd11d87e690a77123
Vladislav Vyskvarko <vyskvarko95@yandex.ru>
2020-11-19 21:38:14 +0300

    Add CMD first realisation

--- FileSumOfBytes.CMD/Program.cs
+++ FileSumOfBytes.CMD/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Runtime.CompilerServices;
+using FileSumOfBytes.Logic.Model;
+using FileOptions = FileSumOfBytes.Logic.Model.FileOptions;
 
 namespace FileSumOfBytes.CMD
 {
@@ -6,7 +11,27 @@ namespace FileSumOfBytes.CMD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //test
+            var tempTestDirectoryPath = "testFolder\\";
+
+            //TODO:write logic for work
+
+            var directory = new DirectoryInfo(tempTestDirectoryPath);
+
+            var fileOptions = new FileOptions();
+            fileOptions.FilesInfo.CollectionChanged += ShowFileInfo;
+
+            fileOptions.AnalyzeFiles(directory);
+            fileOptions.WriteToXml(directory);
+        }
+
+        /// <summary>
+        /// Handles the event of adding to the collection and prints it to the console.
+        /// </summary>
+        private static void ShowFileInfo(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action != NotifyCollectionChangedAction.Add) return;
+            if (e.NewItems[0] is FileHashSum newFile) Console.WriteLine($"{newFile.Name} - {newFile.HashSum}");
         }
     }
 }
